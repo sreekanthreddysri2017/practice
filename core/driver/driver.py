a2894af3963eddb78482688d044a041af32a2d43
MutraSreekanthReddy <sreekanthreddysri2017@gmail.com>
2023-12-22 18:57:47 +0530

    added functions

--- core/driver/driver.py
+++ core/driver/driver.py
@@ -3,5 +3,5 @@ from core.util.main import *
 df = read_csv("../../resource/delta_log.csv", options)
 df.show()
 
-final_df = explode_df(df)
+final_df = explode_df(df,"id_period_end_date","id_scenario_version")
 final_df.show(truncate=False)
--- core/util/main.py
+++ core/util/main.py
@@ -14,15 +14,15 @@ def read_csv(path, options):
 
 
 
-def explode_df(df):
+def explode_df(df,id_period_end_date_col,id_scenario_version_col):
     df1 = df.withColumn("operationParameters", from_json(col("operationParameters"), MapType(StringType(), StringType()))) \
           .withColumn("operationMetrics", from_json(col("operationMetrics"), MapType(StringType(), StringType())))
     filtered_data_df = df1.select("version", "timestamp", "operationParameters.predicate", "operationMetrics.numFiles")
     splited_df = filtered_data_df.select("version", "timestamp", explode(split("predicate", " OR ")).alias("predicate"), "numFiles")
-    explode_df = splited_df.withColumn("id_period_end_date", split(splited_df["predicate"], "AND")[0]) \
-                          .withColumn("id_scenario_version", split(splited_df["predicate"], "AND")[1])
-    final_df = explode_df.withColumn("id_period_end_date", regexp_extract(explode_df["predicate"], "id_period_end_date='(\\d+)'", 1)) \
-                         .withColumn("id_scenario_version", regexp_extract(explode_df["predicate"], "id_scenario_version='(\\d+)'", 1))
+    explode_df = splited_df.withColumn(id_period_end_date_col, split(splited_df["predicate"], "AND")[0]) \
+                          .withColumn(id_scenario_version_col, split(splited_df["predicate"], "AND")[1])
+    final_df = explode_df.withColumn(id_period_end_date_col, regexp_extract(explode_df["predicate"], "id_period_end_date='(\\d+)'", 1)) \
+                         .withColumn(id_scenario_version_col, regexp_extract(explode_df["predicate"], "id_scenario_version='(\\d+)'", 1))
 
     final_df = final_df.drop("predicate")
     return final_df
